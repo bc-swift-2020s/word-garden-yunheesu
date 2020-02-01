42b50d624e681eb215770e95a15eaddff4bf0363
Heesu Yun <Yunheesu@icloud.com>
2020-02-01 15:52:17 -0500

    Section 3.2 - Updating the UI, show: hide the keyboard etc...

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userGuessLabel: UILabel!
+    @IBOutlet weak var guessedLetterField: UITextField!
+    @IBOutlet weak var guessLetterButton: UIButton!
+    @IBOutlet weak var guessCountLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var flowerImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("In viewDidLoad, is guessedLetterField the first responder?", guessedLetterField.isFirstResponder)
+
     }
-
-
+    
+    func updateUIAterGuess(){
+         guessedLetterField.resignFirstResponder()
+         guessedLetterField.text = ""
+        
+    }
+    
+    @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+        print("In guessLetterButtonPressed, is guessedLetterField the first responder before updateUIAfterGuess?", guessedLetterField.isFirstResponder)
+        updateUIAterGuess()
+        print("In guessLetterButotnPressed, is guessedLetterField the first responder after updateUIAfterGuess?", guessedLetterField.isFirstResponder)
+    }
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        print("Hey! The guessedLetterFieldChanged!!!")
+    }
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        print("In doneKeyPressed, is guessedLetterField the first responder before updateUIAfterGuess?", guessedLetterField.isFirstResponder)
+        updateUIAterGuess()
+        print("In doneKeyPressed, is guessedLetterField the first responder after updateUIAfterGuess?", guessedLetterField.isFirstResponder)
+    }
+    
+    
 }
 
